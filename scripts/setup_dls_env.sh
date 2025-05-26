2dd50b24d7549cb4af247850d991b11261619b6b
jmotow <jaroslaw.motowidlo@intel.com>
2025-05-26 14:22:35 +0200

    [DLStreamer] New script for DLS environment configuration (#205)

---
+++ scripts/setup_dls_env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ==============================================================================
+# Copyright (C) 2025 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+# ==============================================================================
+
+export LIBVA_DRIVER_NAME=iHD
+export GST_PLUGIN_PATH=/opt/intel/dlstreamer/build/intel64/Release/lib:/opt/intel/dlstreamer/gstreamer/lib/gstreamer-1.0:/opt/intel/dlstreamer/gstreamer/lib/:
+export LD_LIBRARY_PATH=/opt/intel/dlstreamer/gstreamer/lib:/opt/intel/dlstreamer/build/intel64/Release/lib:/opt/intel/dlstreamer/lib/gstreamer-1.0:/usr/lib:/opt/intel/dlstreamer/build/intel64/Release/lib:/opt/opencv:/opt/openh264:/opt/rdkafka:/opt/ffmpeg:/usr/local/lib/gstreamer-1.0:/usr/local/lib
+export LIBVA_DRIVERS_PATH=/usr/lib/x86_64-linux-gnu/dri
+export GST_VA_ALL_DRIVERS=1
+export PATH=/opt/intel/dlstreamer/gstreamer/bin:/opt/intel/dlstreamer/build/intel64/Release/bin:$PATH
+export GST_PLUGIN_FEATURE_RANK=${GST_PLUGIN_FEATURE_RANK},ximagesink:MAX
