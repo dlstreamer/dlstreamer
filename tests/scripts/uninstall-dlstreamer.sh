8a1d0e728a2c04b0f6484f79153f68ec43fa9f64
Michal Holownia <michal.holownia@intel.com>
2025-08-14 15:12:38 +0200

    [DLStreamer] Modify fedora dockerfile and extract script for rpm extaction (#754)

--- tests/scripts/uninstall-dlstreamer.sh
+++ tests/scripts/uninstall-dlstreamer.sh
@@ -6,22 +6,40 @@
 # ==============================================================================
 set -e
 
+PKG_MGR="${1:-apt}"
+
+if [[ "$PKG_MGR" != "apt" && "$PKG_MGR" != "dnf" ]]; then
+    echo "Unsupported package manager: $PKG_MGR. Use 'apt' or 'dnf'."
+    exit 1
+fi
+
 #Uninstall DLStreamer
 echo "Removing intel-dlstreamer package..."
-sudo apt remove -y intel-dlstreamer
+sudo $PKG_MGR remove -y intel-dlstreamer
 echo " Intel-dlstreamer package uninstalled..."
 echo "Checking if there is any DLStreamer package installed"
-installed_packages=$(sudo apt list --installed 2>/dev/null | grep dlstreamer || true)
+if [ "$PKG_MGR" == "apt" ]; then
+    installed_packages=$(sudo apt list --installed 2>/dev/null | grep dlstreamer || true)
+elif [ "$PKG_MGR" == "dnf" ]; then
+    installed_packages=$(sudo dnf list installed 2>/dev/null | grep dlstreamer || true)
+fi
+
 echo " Found packages: $installed_packages"
 
 #Uninstall openvino packages
 echo "Uninstall openvino packages"
-sudo apt remove -y openvino* libopenvino* python3-openvino*
-sudo apt-get autoremove -y
+sudo $PKG_MGR remove -y openvino* libopenvino* python3-openvino*
+sudo $PKG_MGR autoremove -y
 echo "Packages are uninstalled"
 
-sudo rm -rf /etc/apt/sources.list.d/intel-openvino*.list
-sudo rm -rf /etc/apt/sources.list.d/sed.list
+if [ "$PKG_MGR" == "apt" ]; then
+    echo "Removing OpenVINO repository"
+    sudo rm -rf /etc/apt/sources.list.d/intel-openvino*.list
+    sudo rm -rf /etc/apt/sources.list.d/sed.list
+elif [ "$PKG_MGR" == "dnf" ]; then
+    echo "Removing OpenVINO repository for Fedora"
+    sudo rm -rf /etc/yum.repos.d/intel-openvino*.repo
+fi
 
 if [ -n "$installed_packages" ]; then
     echo "Found the following dlstreamer packages installed:"
