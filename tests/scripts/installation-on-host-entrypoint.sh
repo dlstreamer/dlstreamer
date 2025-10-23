00569e8c5601be4b2dbe8f62733b9f4e40cc94b0
Dawid Michalowski <dawid.michalowski@intel.com>
2025-10-23 10:55:37 +0200

    [DLStreamer] CI reorganization - aligning and merging DLS Pipeline Server to DLS (#1088)
    Signed-off-by: Michalowski, Dawid <dawid.michalowski@intel.com>

--- tests/scripts/installation-on-host-entrypoint.sh
+++ tests/scripts/installation-on-host-entrypoint.sh
@@ -91,8 +91,8 @@ for file in /usr/share/keyrings/intel-graphics*; do
     fi
 done
 
-chmod +x $PREREQUISITES_SCRIPT_PATH/DLS_install_prerequisites.sh
-$PREREQUISITES_SCRIPT_PATH/DLS_install_prerequisites.sh --reinstall-npu-driver=no
+chmod +x "$PREREQUISITES_SCRIPT_PATH"/DLS_install_prerequisites.sh
+"$PREREQUISITES_SCRIPT_PATH"/DLS_install_prerequisites.sh --reinstall-npu-driver=no
 
 # Configure repositories before installation
 echo_color "Starting to configure OpenVINO™ repository access before DL Streamer installation" "blue"
