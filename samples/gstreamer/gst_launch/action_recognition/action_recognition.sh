fe8daef7152725cc3071adbc954451492234ad42
Bujewski, Tomasz <tomasz.bujewski@intel.com>
2025-12-22 13:08:51 +0100

    Extend samples about help info and add missed vapostproc for watermark pipelines

--- samples/gstreamer/gst_launch/action_recognition/action_recognition.sh
+++ samples/gstreamer/gst_launch/action_recognition/action_recognition.sh
@@ -13,6 +13,18 @@ else
   echo "MODELS_PATH: $MODELS_PATH"
 fi
 
+# List help message
+if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+  echo "Usage: $0 [INPUT] [DEVICE] [OUTPUT]"
+  echo ""
+  echo "Arguments:"
+  echo "  INPUT   - Input source (default: Pexels video URL)"
+  echo "  DEVICE  - Device (default: CPU). Supported: CPU, GPU, NPU"
+  echo "  OUTPUT  - Output type (default: file). Supported: file, display, fps, json, display-and-json"
+  echo ""
+  exit 0
+fi
+
 INPUT=${1:-https://videos.pexels.com/video-files/5144823/5144823-uhd_3840_2160_25fps.mp4}
 DEVICE=${2:-CPU}
 OUTPUT=${3:-file} # Supported values: display, fps, json, display-and-json, file
@@ -27,7 +39,7 @@ if [[ -z $INPUT ]]; then
 fi
 
 if [[ $OUTPUT == "display" ]]; then
-  SINK_ELEMENT="gvawatermark ! videoconvert ! gvafpscounter ! autovideosink sync=false"
+  SINK_ELEMENT="vapostproc ! gvawatermark ! videoconvert ! gvafpscounter ! autovideosink sync=false"
 elif [[ $OUTPUT == "fps" ]]; then
   SINK_ELEMENT=" gvafpscounter ! fakesink async=false"
 elif [[ $OUTPUT == "json" ]]; then
@@ -35,7 +47,7 @@ elif [[ $OUTPUT == "json" ]]; then
   SINK_ELEMENT=" gvametaconvert ! gvametapublish file-format=json-lines file-path=output.json ! fakesink async=false"
 elif [[ $OUTPUT == "display-and-json" ]]; then
   rm -f output.json
-  SINK_ELEMENT="gvawatermark ! gvametaconvert ! gvametapublish file-format=json-lines file-path=output.json ! videoconvert ! gvafpscounter ! autovideosink sync=false"
+  SINK_ELEMENT="vapostproc ! gvawatermark ! gvametaconvert ! gvametapublish file-format=json-lines file-path=output.json ! videoconvert ! gvafpscounter ! autovideosink sync=false"
 elif [[ $OUTPUT == "file" ]]; then
   FILE="$(basename ${INPUT%.*})"
   rm -f "action_recognition_${FILE}_${DEVICE}.mp4"
@@ -47,7 +59,7 @@ elif [[ $OUTPUT == "file" ]]; then
     echo "Error - VA-API H.264 encoder not found."
     exit
   fi
-  SINK_ELEMENT="gvawatermark ! gvafpscounter ! ${ENCODER} ! h264parse ! mp4mux ! filesink location=action_recognition_${FILE}_${DEVICE}.mp4"
+  SINK_ELEMENT="vapostproc ! gvawatermark ! gvafpscounter ! ${ENCODER} ! h264parse ! mp4mux ! filesink location=action_recognition_${FILE}_${DEVICE}.mp4"
 else
   echo Error wrong value for OUTPUT parameter
   echo Valid values: "display" - render to screen, "file" - render to file, "fps" - print FPS, "json" - write to output.json, "display-and-json" - render to screen and write to output.json
